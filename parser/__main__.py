9f8d53638a7e2b2ebfa9f1b1d2832cb1d07cdbaa
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-02-02 10:24:15 +0100

    Fixed init and main

--- parser/__main__.py
+++ parser/__main__.py
@@ -16,5 +16,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import sys
+import json
+import logging
 
+from nomad.utils import configure_logging
+from nomad.datamodel import EntryArchive
 from .parser import MsSpecParser
+
+if __name__ == "__main__":
+    configure_logging(console_log_level=logging.DEBUG)
+    archive = EntryArchive()
+    MsSpecParser().parse(sys.argv[1], archive, logging)
+    json.dump(archive.m_to_dict(), sys.stdout, indent=2)
